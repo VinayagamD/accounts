a6d010b245cfb863f357a6ab2d27a0b0e781a4e9
vinay ganesh.d <vinayganeshdxj@gmail.com>
2017-04-30 11:10:32 +0530

    Add DB Details

--- config/app_environment_variables.rb
+++ config/app_environment_variables.rb
@@ -3,8 +3,8 @@ if Rails.env.test? or Rails.env.development?
   ENV['DB_PASSWORD'] = 'drago'
 end
 if Rails.env.production?
-  ENV['DB_NAME'] = 'dcfu8nlb78o7sq'
-  ENV['DB_HOST'] = 'ec2-54-235-248-197.compute-1.amazonaws.com'
-  ENV['DB_USER_NAME'] = 'eveirgevcadplj'
-  ENV['DB_PASSWORD'] = '7e5dcda8fb21154580c0eec3686feeff6123c9fb407df384b7984a9e73ed5ca3'
+  ENV['DB_NAME'] = 'dam30usjk8cj8r'
+  ENV['DB_HOST'] = 'ec2-174-129-227-116.compute-1.amazonaws.com'
+  ENV['DB_USER_NAME'] = 'oxhvmicrajgglm'
+  ENV['DB_PASSWORD'] = '910b0081f3c50c1aab5d5abe12ec51b49427fd0d3b4a340eb4db5cf72c66fb7a'
 end
